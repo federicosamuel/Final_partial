25163961f1d8faa813c3ac3f634e02fdd75f05a7
federicosamuel <145054249+federicosamuel@users.noreply.github.com>
2023-11-11 19:19:56 -0600

    Create Alexa_assistant.py

---
+++ Alexa_assistant.py
@@ -0,0 +1,12 @@
+https://replit.com/join/jlfqcviapi-federicoolmos1
+
+inputt=str(input())
+
+splitted=inputt.split(" ")
+
+alexakounter=splitted.count(str("Alexa"))
+if alexakounter>1:
+  print("Hey! just say my name once!")
+
+else:
+  input("Tell me, how can I help you?")
